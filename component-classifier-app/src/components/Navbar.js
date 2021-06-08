da7dca6f6a523c2973dfa1b704e59736327db3d9
Sumanth Benhur <sbenhur2@gmail>
2021-06-08 21:38:04 +0530

    UI completed

--- component-classifier-app/src/components/Navbar.js
+++ component-classifier-app/src/components/Navbar.js
@@ -16,7 +16,6 @@ import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import * as FaIcons from 'react-icons/fa';
 import HomeIcon from '@material-ui/icons/Home';
 import StorageIcon from '@material-ui/icons/Storage';
 import DataUsageIcon from '@material-ui/icons/DataUsage';
--- component-classifier-app/src/pages/DataVis.js
+++ component-classifier-app/src/pages/DataVis.js
@@ -1,10 +1,100 @@
 import React from "react";
 import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import withOutlier from "../images/withOutlier.png";
+import withoutOutlier from "../images/withoutOutlier.png";
+import lenwidWith from "../images/lenwidWithOutlierOG.png";
+import lenwidWithout from "../images/lenwidWithoutOG.png";
+import clusterWith from "../images/clusterWith.png";
+import clusterWithout from "../images/clusterWithout.png";
 
 function DataVis() {
   return (
     <>
-      <h1>Python Implementation</h1>
+      <h1 class="display-4">Data Visualisation</h1>
+      <div class="row">
+        <figure class="figure col-lg-6">
+          <img
+            src={withOutlier}
+            class="figure-img img-fluid rounded"
+            alt="Original data"
+            style={{
+              maxHeight: "450px",
+            }}
+          />
+          <figcaption class="figure-caption">
+            Image of complete dataset with outliers
+          </figcaption>
+        </figure>
+        <figure class="figure col-lg-6">
+          <img
+            src={withoutOutlier}
+            class="figure-img img-fluid rounded"
+            alt="Original data"
+            style={{
+              maxHeight: "450px",
+            }}
+          />
+          <figcaption class="figure-caption">
+            Image of complete dataset without outliers
+          </figcaption>
+        </figure>
+      </div>
+      <div class="row">
+        <figure class="figure col-lg-6">
+          <img
+            src={lenwidWith}
+            class="figure-img img-fluid rounded"
+            alt="length vs width with outliers"
+            style={{
+              maxHeight: "275px",
+            }}
+          />
+          <figcaption class="figure-caption">
+            Image of complete dataset with outliers (width vs length)
+          </figcaption>
+        </figure>
+        <figure class="figure col-lg-6">
+          <img
+            src={lenwidWithout}
+            class="figure-img img-fluid rounded"
+            alt="length vs width without outliers"
+            style={{
+              maxHeight: "275px",
+            }}
+          />
+          <figcaption class="figure-caption">
+            Image of complete dataset without outliers (width vs length)
+          </figcaption>
+        </figure>
+      </div>
+      <div class="row">
+        <figure class="figure col-lg-6">
+          <img
+            src={clusterWith}
+            class="figure-img img-fluid rounded"
+            alt="length vs width cluster with outliers"
+            style={{
+              maxHeight: "275px",
+            }}
+          />
+          <figcaption class="figure-caption">
+            Image of clustered data with outliers (width vs length)
+          </figcaption>
+        </figure>
+        <figure class="figure col-lg-6">
+          <img
+            src={clusterWithout}
+            class="figure-img img-fluid rounded"
+            alt="length vs width cluster without outliers"
+            style={{
+              maxHeight: "275px",
+            }}
+          />
+          <figcaption class="figure-caption">
+            Image of clustered data without outliers (width vs length)
+          </figcaption>
+        </figure>
+      </div>
     </>
   );
 }
